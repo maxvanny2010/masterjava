8ec46a339318ddad90153adf89feae2261ab7200
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2021-09-17 11:15:51 +0300

    5_4_HW4_parallel.patch

--- persist/src/main/java/ru/javaops/masterjava/persist/dao/UserDao.java
+++ persist/src/main/java/ru/javaops/masterjava/persist/dao/UserDao.java
@@ -1,12 +1,19 @@
 package ru.javaops.masterjava.persist.dao;
 
 import com.bertoncelj.jdbi.entitymapper.EntityMapperFactory;
-import org.skife.jdbi.v2.sqlobject.*;
+import one.util.streamex.IntStreamEx;
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
+import org.skife.jdbi.v2.sqlobject.SqlBatch;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.Transaction;
 import org.skife.jdbi.v2.sqlobject.customizers.BatchChunkSize;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
+import ru.javaops.masterjava.persist.DBIProvider;
 import ru.javaops.masterjava.persist.model.User;
-
-import java.util.List;
+import java.util.*;
 
 @RegisterMapperFactory(EntityMapperFactory.class)
 public abstract class UserDao implements AbstractDao {
@@ -19,6 +26,16 @@ public abstract class UserDao implements AbstractDao {
             insertWitId(user);
         }
         return user;
+    }
+
+    @SqlQuery("SELECT nextval('user_seq')")
+    abstract int getNextVal();
+
+    @Transaction
+    public int getSeqAndSkip(int step) {
+        int id = getNextVal();
+        DBIProvider.getDBI().useHandle(h -> h.execute("SELECT setval('user_seq', " + (id + step) + ")"));
+        return id;
     }
 
     @SqlUpdate("INSERT INTO users (full_name, email, flag) VALUES (:fullName, :email, CAST(:flag AS USER_FLAG)) ")
@@ -37,8 +54,17 @@ public abstract class UserDao implements AbstractDao {
     public abstract void clean();
 
     //    https://habrahabr.ru/post/264281/
-    @SqlBatch("INSERT INTO users (full_name, email, flag) VALUES (:fullName, :email, CAST(:flag AS USER_FLAG))" +
+    @SqlBatch("INSERT INTO users (id, full_name, email, flag) VALUES (:id, :fullName, :email, CAST(:flag AS USER_FLAG))" +
             "ON CONFLICT DO NOTHING")
 //            "ON CONFLICT (email) DO UPDATE SET full_name=:fullName, flag=CAST(:flag AS USER_FLAG)")
     public abstract int[] insertBatch(@BindBean List<User> users, @BatchChunkSize int chunkSize);
+
+
+    public List<String> insertAndGetConflictEmails(List<User> users) {
+        int[] result = insertBatch(users, users.size());
+        return IntStreamEx.range(0, users.size())
+                .filter(i -> result[i] == 0)
+                .mapToObj(index -> users.get(index).getEmail())
+                .toList();
+    }
 }
