e70e7abdaab5886d66e89e8b97cad5d17a045a18
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2021-09-27 16:34:17 +0300

    set HW database

--- sql/lb_apply.bat
+++ sql/lb_apply.bat
@@ -2,7 +2,7 @@ set LB_HOME=C:\apps\masterjava\liquibase
 call %LB_HOME%\liquibase.bat --driver=org.postgresql.Driver ^
 --classpath=%LB_HOME%\lib ^
 --changeLogFile=databaseChangeLog.sql ^
---url="jdbc:postgresql://localhost:5432/masterjava" ^
+--url="jdbc:postgresql://localhost:5432/masterjavaHW" ^
 --username=postgres ^
 --password=password ^
 migrate
