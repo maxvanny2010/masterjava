e8972a1ce95dd52676c0522f9a9c59b2bc220383
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2021-10-21 15:10:08 +0300

    8_5_soap_exceptions.patch

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -1,11 +1,15 @@
 package ru.javaops.masterjava.service.mail;
 
 import com.google.common.collect.ImmutableSet;
+import lombok.extern.slf4j.Slf4j;
+import ru.javaops.web.WebStateException;
 
+@Slf4j
 public class MailWSClientMain {
-    public static void main(String[] args) {
-        MailWSClient.sendToGroup(
+    public static void main(String[] args) throws WebStateException {
+        String state = MailWSClient.sendToGroup(
                 ImmutableSet.of(new Addressee("To <herokuboot@gmail.com>")),
-                ImmutableSet.of(new Addressee("Copy <herokuboot@gmail.com>")), "Subject", "Body");
+                ImmutableSet.of(new Addressee("Bad Email<bad_email.ru>")), "Subject", "Body");
+        System.out.println(state);
     }
 }
