c4d8dded1b84a6590c789f800df61f84eec79300
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2021-10-29 18:57:56 +0300

    10_2_HW9_host_config.patch

--- services/common-ws/src/main/java/ru/javaops/masterjava/web/handler/SoapLoggingHandlers.java
+++ services/common-ws/src/main/java/ru/javaops/masterjava/web/handler/SoapLoggingHandlers.java
@@ -135,8 +135,8 @@ public abstract class SoapLoggingHandlers extends SoapBaseHandler {
 
     public static class ServerHandler extends SoapLoggingHandlers {
 
-        public ServerHandler() {
-            super(Level.INFO);
+        public ServerHandler(Level loggingLevel) {
+            super(loggingLevel);
         }
 
         @Override
